70d53fdf968e83347fcdef5c26d30a5be6bee049
kretzsch <wkretzsch@gmail.com>
2014-02-20 16:15:32 +0000

    added test to hapfuse for correct phasing

--- testing/tests/20-bigPhase.pl
+++ testing/tests/20-bigPhase.pl
@@ -2,20 +2,28 @@
 use Test::More tests => 1;
 use Test::Files;
 use File::Spec;
+use File::Basename;
+use File::Path qw(make_path remove_tree);
 
 SKIP: {
-      skip "vcftools does not exist on system", 1 unless qx/which vcftools/ =~ m/.*vcftools$/;
+    skip "vcftools does not exist on system", 1
+      unless qx/which vcftools/ =~ m/.*vcftools$/;
 
-      my $expected_file =
-        File::Spec->catfile(qw/ .. samples test01 test.1Col.hapfuse.expected.vcf /);
-      my $resultsDir = qw/results/;
-      my $results_file =         File::Spec->catfile($resultsDir, q|test20.hapfuse.vcf|);
-      system "../bin/hapfuse -o $results_file ../samples/test20/*.vcf";
+    my $resultsDir = qw|results/test20|;
+    make_path($resultsDir);
+    my $expected_file =
+      File::Spec->catfile(qw/ .. samples test20 expected.impute.hap /);
+    my $resultsName = "test20.hapfuse";
+    my $results_file =
+      File::Spec->catfile( $resultsDir, $resultsName . q/.impute.hap/ );
 
-      # pull haplotypes out of vcf
-      system "vcftools --vcf $results_file --IMPUTE --out basename -s .vcf $results_file";
-#      compare_ok( $expected_file, $results_file, "hapfuse phases" );
-  };
+    my $results_vcf =
+      File::Spec->catfile( $resultsDir, $resultsName . q/.vcf/ );
+    system "../bin/hapfuse -o $results_vcf ../samples/test20/*.bin.vcf";
 
-
+    # pull haplotypes out of vcf
+    system
+      "vcftools --vcf $results_vcf --IMPUTE --out ${resultsDir}/$resultsName";
+    compare_ok( $expected_file, $results_file, "hapfuse phases" );
+}
 

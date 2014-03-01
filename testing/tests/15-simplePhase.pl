b45b3ff5acc79e9a5d2798d537085a19b44b20d6
kretzsch <wkretzsch@gmail.com>
2014-03-01 19:52:36 +0000

    added APP simple test

---
+++ testing/tests/15-simplePhase.pl
@@ -0,0 +1,34 @@
+#!/bin/perl
+use Test::More;
+use Test::Files;
+use File::Spec;
+use File::Basename;
+use File::Path qw(make_path remove_tree);
+
+unless ( qx/which vcftools/ =~ m/.*vcftools$/ ) {
+    plan skip_all => "vcftools does not exist on system";
+}
+
+# this test tests the GP field parsing
+my $testTag    = 'test15';
+
+my $resultsDir = "results/$testTag";
+make_path($resultsDir);
+my $expectedHap = File::Spec->catfile( qw/ .. samples /, $testTag,
+"$testTag.expected.chr20_20000120_20153108.STv1.2.17.C500.B0.i0.m20.K100.t2.f.q0.05.h.s.bin.vcf.hap"
+);
+
+my $resultsName = "$testTag.hapfuse";
+my $resultsHap =
+  File::Spec->catfile( $resultsDir, $resultsName . q/.impute.hap/ );
+
+my $results_vcf = File::Spec->catfile( $resultsDir, $resultsName . q/.vcf/ );
+system
+  "../bin/hapfuse -o $results_vcf ../samples/$testTag/$testTag.chr20*.bin.vcf";
+
+# pull haplotypes out of vcf
+system
+"vcftools --vcf $results_vcf --IMPUTE --out ${resultsDir}/$resultsName";
+
+compare_ok( $expectedHap,    $resultsHap,    "hapfuse phases" );
+
